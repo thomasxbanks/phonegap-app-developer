4bf48fc377c3e5e67bc265969617622ca7d5d026
thomasxbanks <thomasxbanks@gmail.com>
2019-08-05 15:30:23 +0100

    Adds videos for testing

--- www/index.js
+++ www/index.js
@@ -2,6 +2,7 @@ const header = document.querySelector('header')
 const footer = document.querySelector('footer')
 const main = document.querySelector('.content_wrapper')
 const title = document.querySelector('h1')
+let video
 
 const typewriter = (el) => {
   const text = el.innerHTML
@@ -9,7 +10,6 @@ const typewriter = (el) => {
   const len = inputArr.length - 1
   let count = 0
   const delay = (1000 / len)
-  el.dataset.content = text
   el.innerHTML = ''
   const interval = setInterval(() => {
     if (count === len) {
@@ -19,20 +19,19 @@ const typewriter = (el) => {
   }, delay)
 }
 
-const fadeIn = (el) => {
-  el.dataset.active = true
-}
-
 const resetAnimation = (el) => {
-  el.classList.remove('can-animate')
+  el.classList.add('no-animation')
   el.dataset.active = false
-  el.innerHTML = el.dataset.content
+  setTimeout(() => {
+    el.classList.remove('no-animation')
+  }, 1000)
+  video.pause()
 }
 
 const runAnimation = (el) => {
-  el.classList.add('can-animate')
-  fadeIn(el)
+  el.dataset.active = true
   typewriter(el)
+  video.play()
 }
 
 const onPause = () => {
@@ -40,30 +39,34 @@ const onPause = () => {
 }
 
 const onResume = () => {
-  setTimeout(() => {
-    runAnimation(title)
-  }, 2000)
+  runAnimation(title)
 }
 
 const onDeviceReady = () => {
-  runAnimation(title)
   
   const content = [
+    `<video autoplay muted loop poster="./images/stat_01.png">
+      <source src="./videos/stat_01.mp4" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>`,
     `<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id, nostrum neque dicta corporis cupiditate ex? Pariatur error ipsum tenetur alias? Ipsa placeat exercitationem cumque maiores, facilis corporis iste earum mollitia!</p>`,
     `<p>Cumque quas nemo mollitia iste alias? Enim nulla nobis ea, quo reiciendis sit nam! Quibusdam est possimus hic minima quam facilis nulla vel. Officiis veniam iusto voluptatibus est ipsum voluptas?</p>`,
     `<p>Doloribus velit dolores perspiciatis optio at, animi a temporibus autem cum quis aperiam, ab dolor nesciunt modi repellat veritatis ipsam consequuntur adipisci aliquam ea molestias reiciendis! Ducimus fugiat minus tenetur!</p>`,
     `<p>Repellat id cupiditate vel voluptates, itaque nobis, nostrum ut impedit vero similique expedita nulla quia corporis. Similique vitae ratione iure quia! Minus dicta architecto iusto libero velit nobis excepturi quisquam!</p>`,
-    `<figure><img src="./images/768.jpeg" /><figcaption>What an adorable kitten!</figcaption></figure>`,
     `<p>Reiciendis unde sunt laudantium provident? Quas sunt fuga dignissimos minus velit, aliquid quis laborum vero, id obcaecati sit sed consequatur blanditiis cum accusantium dicta reprehenderit quae illum? Ipsum, cupiditate eum.</p>`,
     `<p>Consequatur placeat, ipsum corporis blanditiis ipsa ratione quos voluptatum iusto, ea quisquam ad similique nihil esse amet, eligendi illum molestiae a nisi. Neque hic id assumenda beatae corrupti ipsa ab.</p>`,
     `<p>Porro ab ea, consequatur omnis, odit recusandae libero quae repellat pariatur asperiores non tempore. Animi consequatur quam similique, magnam officiis neque eaque excepturi. Eveniet laborum non eaque, incidunt accusantium veniam!</p>`,
     `<p>Voluptate quibusdam animi doloremque odio voluptatum harum quia unde quos, cum maiores dolores soluta esse sapiente facere magnam. Vitae itaque obcaecati necessitatibus et inventore nesciunt maiores dolorum cumque ut autem?</p>`,
     `<p>Nihil, mollitia odio corrupti consequatur distinctio ab blanditiis itaque quisquam, dolorum labore sit, nisi dolore veritatis aut. Aut earum eveniet rerum. Fuga quasi quos fugiat obcaecati facere consequatur eum quaerat.</p>`,
+    `<figure><img data-src="https://media0.giphy.com/media/zQfX46nOhYzjG/giphy.gif?cid=790b76115d4837b277576d3059144ad5&rid=giphy.gif" /><figcaption>You want a hug?</figcaption></figure>`,
     `<p>Modi assumenda, iure ipsum veniam, aut libero doloremque impedit molestiae repudiandae quis praesentium optio possimus. Quasi voluptatibus fugit minima neque. Sit laborum autem minima nostrum veniam deserunt, aut id corrupti.</p>`,
   ]
   
   content.map(p => main.innerHTML += p)
-
+  
+  video = document.querySelector('video')
+  
+  runAnimation(title)
 }
 
 const onLoad = () => {
@@ -72,7 +75,7 @@ const onLoad = () => {
   const footerHeight = window.getComputedStyle(footer, null).getPropertyValue('height')
   main.style.minHeight = `calc(100vh - ${headerHeight} - ${footerHeight} - 1rem)`
   document.addEventListener('deviceready', onDeviceReady, false)
-  // onDeviceReady()
+  onDeviceReady()
 }
 
 document.addEventListener("pause", onPause, false);
